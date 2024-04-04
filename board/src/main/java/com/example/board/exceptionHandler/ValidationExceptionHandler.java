809f4fc1abb90f382de805063865b20e2c3316ef
hhhunnn <gusdk2090@naver.com>
2024-04-04 15:48:17 +0900

    2024.04.04

---
+++ board/src/main/java/com/example/board/exceptionHandler/ValidationExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.board.exceptionHandler;
+
+import org.springframework.http.HttpMessage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.board.dto.response.ResponseDto;
+
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(value={HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ResponseDto> validationExceptionHandeler(Exception exception) {
+        exception.printStackTrace();
+        return ResponseDto.validationFail();
+    }
+    
+}
+
+
+// 이부분(AuthController) 예외 
+// public ResponseEntity<ResponseDto> signUp(
+//         @RequestBody @Valid SignUpRequestDto requestBody
+//     ) {
